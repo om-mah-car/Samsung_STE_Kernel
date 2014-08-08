e41b6a85bf73b783490cd481852c1a513c194aa9
Chrono Monochrome <chrono.monochrome@gmail.com>
2014-08-08 20:44:26 +0400

    fix build errors

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -1580,14 +1580,9 @@ static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 		if (EXFAT_I(inode)->mmu_private < (offset + iov_length(iov, nr_segs)))
 			return 0;
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)
-	ret = blockdev_direct_IO(rw, iocb, inode, iov,
-					offset, nr_segs, exfat_get_block);
-#else
+
         ret = blockdev_direct_IO(rw, iocb, inode, inode->i_sb->s_bdev, iov,
 					offset, nr_segs, exfat_get_block, NULL);
-#endif
-
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,34)
 	if ((ret < 0) && (rw & WRITE))
 		exfat_write_failed(mapping, offset+iov_length(iov, nr_segs));
